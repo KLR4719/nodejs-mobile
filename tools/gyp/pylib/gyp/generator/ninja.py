b49501d957740da1428ecc6e74110326ec9a31e8
Vincent Breitmoser <look@my.amazin.horse>
2025-10-22 10:45:50 +0200

    nodejs-mobile: tools/gyp: adapt
    Todo: split up and/or better commit description?

--- tools/gyp/pylib/gyp/generator/ninja.py
+++ tools/gyp/pylib/gyp/generator/ninja.py
@@ -42,7 +42,8 @@ generator_default_variables = {
     # the start of a string, while $| is used for variables that can appear
     # anywhere in a string.
     "INTERMEDIATE_DIR": "$!INTERMEDIATE_DIR",
-    "SHARED_INTERMEDIATE_DIR": "$!PRODUCT_DIR/gen",
+    # nodejs-mobile patch: allow multiple rules to generate the same object (host and target)
+    "SHARED_INTERMEDIATE_DIR": "$!PRODUCT_DIR/$|OBJ/gen",
     "PRODUCT_DIR": "$!PRODUCT_DIR",
     "CONFIGURATION_NAME": "$|CONFIGURATION_NAME",
     # Special variables that may be used by gyp 'rule' targets.
@@ -282,6 +283,13 @@ class NinjaWriter:
 
         CONFIGURATION_NAME = "$|CONFIGURATION_NAME"
         path = path.replace(CONFIGURATION_NAME, self.config_name)
+
+        # nodejs-mobile patch: allow multiple rules to generate the same object (host and target)
+        obj = "obj"
+        if self.toolset != "target":
+            obj += "." + self.toolset
+
+        path = path.replace("$|OBJ", obj)
 
         return path
 
